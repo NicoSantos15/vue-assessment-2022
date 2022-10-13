c12c53e2ec35c47fe24d634e8234898ffed2ac2f
Nico Santos <nicos@citb.org.au>
2022-10-13 19:30:36 +1030

    Challenge3: Fixed capitalised letter of variable.

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/challenge-1">Challenge 1</router-link> | 
-      <router-link to="/challenge-2">Challenge 2</router-link> | 
+      <router-link to="/challenge-1">Challenge 1</router-link> |
+      <router-link to="/challenge-2">Challenge 2</router-link> |
       <router-link to="/challenge-3">Challenge 3</router-link> |
       <router-link to="/final">Final</router-link>
     </div>
-    <router-view/>
+    <router-view />
+    <div class="flower">
+      <img src="https://images.unsplash.com/photo-1613539246066-78db6ec4ff0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80" 
+      alt="Flower1">
+    </div>
   </div>
 </template>
 
@@ -31,5 +35,18 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+img {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.flower {
+  height: 500px;
+  width: 330px;
+  position: absolute;
+  bottom: 0%;
+  left: 0%;
 }
 </style>
--- src/views/Challenge3.vue
+++ src/views/Challenge3.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="challenge">
     <h1>Bug Challenge 3</h1>
-    <component-2 :Msg='msg' />
+    <component-2 :msg='msg' />
+    <!-- This could be implemented as a global 
+    <div class="flower">
+      <img src="https://images.unsplash.com/photo-1613539246066-78db6ec4ff0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80" 
+      alt="Flower1">
+    </div>
+    -->
   </div>
 </template>
 
 <script>
-  import Component2 from '@/components/Component2'
+import Component2 from '@/components/Component2'
 
-  export default {
-    name: 'Challenge2',
-    components: {
-      Component2
-    },
-    data() {
-      return {
-        msg: [
-          'Congrats you have discovered the issue',
-          'Don\'t forget to commit this solution'
-        ]
-      }
+export default {
+  name: 'Challenge2',
+  components: {
+    Component2
+  },
+  data() {
+    return {
+      msg: [
+        'Congrats you have discovered the issue',
+        'Don\'t forget to commit this solution'
+      ]
     }
   }
+}
 </script>
