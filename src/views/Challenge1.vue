0950dc1ff9d01dd84db56c401c0449ad69b73df9
Nico Santos <nicos@citb.org.au>
2022-10-13 19:27:00 +1030

    Challenge1: Fixed data and some word inconsistency

--- src/views/Challenge1.vue
+++ src/views/Challenge1.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="challenge">
     <h1>Bug Challenge 1</h1>
-    <p>{{ msg }}</p>
+    <p>{{ this.msg }}</p>
+    <p>{{ this.msg2 }}</p>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Challenge1',
-    data: {
+export default {
+  name: 'Challenge1',
+  data() {
+    return {
       msg: 'Congrats you have discovered the issue',
-      msg2: 'Don\t forget to commit this solutiom'
+      msg2: 'Don\'t forget to commit this solution'
     }
   }
+}
 </script>
